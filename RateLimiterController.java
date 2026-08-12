d270a588c6974ceddd22c0f0212d1d976568bcb9
Yesh2344 <125935833+Yesh2344@users.noreply.github.com>
2026-08-12 08:01:57 +0000

    wip: trying a cleaner approach

--- RateLimiterController.java
+++ RateLimiterController.java
@@ -3,6 +3,7 @@ package com.example.ratelimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+// leaving a note for later
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
